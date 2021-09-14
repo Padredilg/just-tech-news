e1a3b59a5508bf71211ded7fcaf07cb6cb1996ef
Padredilg <padredilg@knights.ucf.edu>
2021-09-14 14:48:46 -0400

    render information according to whether user is logged in or not

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -98,12 +98,16 @@ router.post('/', (req, res) => {
 // PUT /api/posts/upvote
 //needs to be bfore /:id, or express will think upvote is an id param
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    // make sure the session exists first
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedVoteData => res.json(updatedVoteData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 //update a post
